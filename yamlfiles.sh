5be71d4a0216665730c26f9b0541b2c6e81b001b
Stephen Burke <se7enack@users.noreply.github.com>
2023-12-19 10:17:57 -0500

    Add files via upload

---
+++ yamlfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+file=vars.yaml
+echo "---" > $file
+for i in $(find . -maxdepth 1 -type f | awk -F '/' '{print $2}' | grep -v .DS_Store | grep png); do
+    name_fix=$(echo "$i" | sed 's/ /_SPACE_/g')
+    echo "${name_fix}: $(cat $i | base64)" >> $file
+done
+IFS=$SAVEIFS
