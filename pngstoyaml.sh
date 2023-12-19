eb1a5953a4b1ca8191decd7d9e888f0279ed8070
Stephen Burke <se7enack@users.noreply.github.com>
2023-12-19 10:29:58 -0500

    pngstoyaml.sh

--- pngstoyaml.sh
+++ pngstoyaml.sh
@@ -2,7 +2,7 @@
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-file=vars.yaml
+file=pngs.yaml
 echo "---" > $file
 for i in $(find . -maxdepth 1 -type f | awk -F '/' '{print $2}' | grep -v .DS_Store | grep png); do
     name_fix=$(echo "$i" | sed 's/ /_SPACE_/g')
