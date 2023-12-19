122ed844666b31c5268f28200a137e2a01e48b53
Stephen Burke <se7enack@users.noreply.github.com>
2023-12-19 17:30:00 -0500

    yamltopngs.py

--- yamltopngs.py
+++ yamltopngs.py
@@ -11,3 +11,5 @@ with open('pngs.yaml', 'r') as f:
         with open(key, 'wb') as file_to_save:
             decoded_image_data = base64.decodebytes(value_bytes)
             file_to_save.write(decoded_image_data)
+            file_to_save.close()
+f.close()
